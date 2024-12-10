b54de41feb775c986730c02b4b8170564e06aeeb
MichaelVirks <michael.virks@newcubator.com>
2024-12-10 22:05:12 +0100

    jwt integration

---
+++ backend/estimo/src/main/java/de/esteemo/estimo/EstimoApplication.java
@@ -0,0 +1,13 @@
+package de.esteemo.estimo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EstimoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EstimoApplication.class, args);
+	}
+
+}
